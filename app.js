ce2cb81d9ce56abb704deefad9f7314f0828d23c
Brennan Kolotinsky <brennankolotinsky@Brennans-MacBook-Pro.local>
2020-09-22 16:26:08 -0400

    grabbed all of the file names

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-console.log("here");
+var fs = require('fs'); // create file server object to access files
+var files = fs.readdirSync(process.cwd() + '/Photos_To_Review'); // process.cwd() is a node.js method to grab our current directory
+
+console.log(files);

607ad30865f120736eaaa6158477ef00c81dcf90
Brennan Kolotinsky <brennankolotinsky@Brennans-MacBook-Pro.local>
2020-09-22 16:42:55 -0400

    added exif to get file metadata see: https://github.com/gomfunkel/node-exif

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+// research shows this package will display ISO data: https://github.com/gomfunkel/node-exif
+
 var fs = require('fs'); // create file server object to access files
 var files = fs.readdirSync(process.cwd() + '/Photos_To_Review'); // process.cwd() is a node.js method to grab our current directory
 
-console.log(files);
+console.log(files[0]);

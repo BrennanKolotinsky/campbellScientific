8ba54a09806811c752cab8f101bcb349de74ef96
Brennan Kolotinsky <brennankolotinsky@Brennans-MacBook-Pro.local>
2020-09-22 17:58:39 -0400

    structured and commented code

--- app.js
+++ app.js
@@ -1,37 +1,30 @@
-// research shows this package will display ISO data: https://github.com/gomfunkel/node-exif
+// This guide shows how to get ISO data from an image path: https://www.beyondjava.net/how-to-extract-exif-metadata-from-an-image
 
-var fs = require('fs'); // create file server object to access files
-var imageFiles = fs.readdirSync(process.cwd() + '/Photos_To_Review'); // process.cwd() is a node.js method to grab our current directory
-// note: imageFiles in an array containing all of the images names
+function main() {
+	console.log("Let's begin running this mini app!");
+	var fs = require('fs'); // create file server object to access files
+	var imageFiles = getImageFileNames(fs); // imageFiles in an array containing all of the images names
 
-let isoVal = []; // this is an array we will make, we will store all of the ISO values in the same order the images are. Later we will sort both from highest to lowest!
+	var isoVal = getISOVals(fs, imageFiles); // this is an array we will make, we will store all of the ISO values in the same order the images are. Later we will sort both from highest to lowest!
+	console.log(isoVal);
+}
 
+function getImageFileNames(fs) {
+	return fs.readdirSync(process.cwd() + '/Photos_To_Review'); // process.cwd() is a node.js method to grab our current directory
+}
 
-// Image_846.jpg
+function getISOVals(fs, imageFiles) {
+	var exif = require('exif-parser'); // this is our node package that we will use to read the ISO data!
+	var isoVal = [];
+	for (var i = 0; i < imageFiles.length; i++) {
+		let imageName = process.cwd() + '/Photos_To_Review/' + imageFiles[i];
+		const buffer = fs.readFileSync(imageName);
+		const parser = exif.create(buffer);
+		const result = parser.parse();
+		isoVal[i] = result.tags.ISO; // add this ISO value to the array
+	}
 
-var exif = require('exif-parser');
+	return isoVal;
+}
 
-const buffer = fs.readFileSync('Image_846.jpg');
-const parser = exif.create(buffer);
-const result = parser.parse();
-
-console.log(result);
-// var metadata = exif(buffer);
-
-// console.log(metadata);
-
-
-
-// for (var i = 0; i < 1; i++) { //imageFiles.length
-// 	let imgName = 'Photos_To_Review/' + imageFiles[i];
-// 	try {
-// 	    new ExifImage({ image : imgName }, function (error, exifData) {
-// 	        if (error)
-// 	            console.log('Error: '+error.message);
-// 	        else
-// 	            console.log(exifData); // Do something with your data!
-// 	    });
-// 	} catch (error) {
-// 	    console.log('Error: ' + error.message);
-// 	}
-// }
+main(); // call our main function
